7590f42191fa51fcd59a2d9971aacf5cd0174c89
John <johnmulvey48@yahoo.com>
2022-01-27 13:39:39 -0500

    git@github.com:Jmulvez/phase-1-practice-dog-show.git

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+
+fetch('http://localhost:3000/dogs')
+.then(function(response) {
+    return response.json();
+}
+
+fetch('http://localhost:3000/dogs') {
+    method: 'PATCH',
+    body: JSON.stringify,
+    headers: {
+        "content-type:" "application.json"
+    }
+})
+.then(response => response.json())
+.then(console.log(json))
